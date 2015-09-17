8f8bcc6cc28a5b3b97eb379f2a659e8d04c76a07
omid <platipino@gmail.com>
2015-09-17 16:26:33 +0430

    proguard rule for retrofit added

---
+++ app/src/main/java/io/bottel/http/BottelService.java
@@ -0,0 +1,8 @@
+package io.bottel.http;
+
+/**
+ * Created by Omid on 9/17/2015.
+ */
+public interface BottelService {
+
+}
---
+++ app/src/main/java/io/bottel/http/BottelServiceDefinition.java
@@ -0,0 +1,7 @@
+package io.bottel.http;
+
+/**
+ * Created by Omid on 9/17/2015.
+ */
+public interface BottelServiceDefinition {
+}
