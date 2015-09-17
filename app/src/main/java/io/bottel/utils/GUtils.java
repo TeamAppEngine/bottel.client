700ea3ad89ebb5a24e47c08b3647e62eaae67229
omid <platipino@gmail.com>
2015-09-17 17:02:51 +0430

    added GUtils

--- app/src/main/java/io/bottel/utils/GUtils.java
+++ app/src/main/java/io/bottel/utils/GUtils.java
@@ -4,4 +4,7 @@ package io.bottel.utils;
  * Created by Omid on 9/17/2015.
  */
 public class GUtils {
+    public static boolean isNullOrEmpty(Object o) {
+        return o == null || o.toString().isEmpty();
+    }
 }
--- app/src/main/java/io/bottel/voip/VoipService.java
+++ app/src/main/java/io/bottel/voip/VoipService.java
@@ -1,7 +1,46 @@
 package io.bottel.voip;
 
+import android.content.Context;
+
 /**
  * Created by Omid on 9/17/2015.
  */
-public class VoipService {
+public abstract class VOIPService {
+
+    private boolean isSetup = false;
+
+    public void init(Context context) {
+        isSetup = true;
+    }
+
+    public abstract void connect(Context context);
+
+    public void hangUp(Context context) {
+//        if (context != null)
+//            PresencePersistent.setCurrentState(context, PresencePersistent.State.AVAILABLE);
+    }
+
+    public abstract boolean isOnCall();
+
+    public boolean isSetup() {
+        return isSetup;
+    }
+
+    public abstract void loginAsync(String username, String password);
+
+    public abstract void loginAsync(Context Context, String username, String password);
+
+    public abstract void call(String receipt);
+
+    public abstract boolean isConnected();
+
+    public abstract void hold();
+
+    public abstract void answerCall();
+
+    public abstract void rejectCall();
+
+    public abstract void resume();
+
+    public abstract void signOut();
 }
