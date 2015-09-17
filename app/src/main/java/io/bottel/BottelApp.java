20abcb4d6d3a1147dc8a45c745fe906453f8ffe8
omid <platipino@gmail.com>
2015-09-17 18:27:28 +0430

    added bottel application class

--- app/src/main/java/io/bottel/BottelApp.java
+++ app/src/main/java/io/bottel/BottelApp.java
@@ -1,7 +1,47 @@
 package io.bottel;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+
+import io.bottel.services.KeepAliveConnectionService;
+import io.bottel.services.PresenceAlarm;
+import io.bottel.utils.CallServiceManager;
+import io.bottel.voip.VOIPService;
+
 /**
  * Created by Omid on 9/17/2015.
  */
-public class BottelApp {
+public class BottelApp extends Application {
+
+
+    private static BottelApp instance;
+//    VOIPService callService;
+
+    public static BottelApp getInstance() {
+        return instance;
+    }
+
+    public static Context getAppContext() {
+        return instance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Intent intent = new Intent(this, KeepAliveConnectionService.class);
+        startService(intent);
+
+        instance = this;
+
+
+        // start presence report service
+        PresenceAlarm.init(this);
+
+    }
+
+    public VOIPService getCallService() {
+        return CallServiceManager.getCallService();
+    }
 }
