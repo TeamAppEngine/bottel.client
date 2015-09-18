ca43f020424d6c6325df6482e0655f5dfc036036
Ali Motameni <motameni1371@gmail.com>
2015-09-18 12:26:50 +0430

    Incoming call layout designed

--- app/src/main/java/io/bottel/views/activities/maps/MapsActivity.java
+++ app/src/main/java/io/bottel/views/activities/maps/MapsActivity.java
@@ -57,7 +57,7 @@ public class MapsActivity extends FragmentActivity {
 
     public static List<LocalPin> localPinArrayList = new ArrayList<>();
     public List<Marker> markers = new ArrayList<>();
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
