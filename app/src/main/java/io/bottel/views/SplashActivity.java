d355223aea9f47620e3a293d57a1d67f2c9f89fc
omid <platipino@gmail.com>
2015-09-18 09:47:05 +0430

    changed default page to mapsactivity

--- app/src/main/java/io/bottel/views/SplashActivity.java
+++ app/src/main/java/io/bottel/views/SplashActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import io.bottel.R;
+import io.bottel.views.activities.maps.MapsActivity;
 
 public class SplashActivity extends Activity {
 
-    /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    /**
+     * Duration of wait
+     **/
+    final int SPLASH_DISPLAY_LENGTH = 3000;
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -20,11 +25,11 @@ public class SplashActivity extends Activity {
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this, SignUpActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, MapsActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
